19da6a53ea8fc7e9a56baec2fad5c380da416a56
Eve Shi <qianyiwork@gmail.com>
2018-02-20 23:14:26 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,53 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-
-class menuContent extends React.Component {
-  constructor(props){
-
-  }
-
-  render(){
-    return(
-      <li></li>
-    )
-  }
-}
-
-class coolHead extends React.Component {
-  constructor(props){
-    this.state = {
-      headText: "Random Boy",
-      menuFlag: false,
-    };
-  }
-
-  menu(){
-    if(this.state.menuFlag){
-      this.setState({
-        menuFlag: false
-      });
-    }else{
-      this.setState({
-        menuFlag: true
-      });
-    }
-  }
-
-  render(){
-    return(
-      <div class="menu">
-        <button onClick={this.menu}>
-          <image src={menuImg}></image>
-        </button>
-        <div class="menuContent" style={menuFlag? "display:none":""}>
-          <form>
-            <ul><menuContent></menuContent></ul>
-          </form>
-        </div>
-      </div>
-    )
-  }
-}
+import Person from './person/person';
 
 class App extends React.Component {
   constructor(props){
@@ -58,7 +11,7 @@ class App extends React.Component {
       numberGiven: 0,
     };
     this.inputPeople = this.inputPeople.bind(this);
-    this.submitData = this.submitData.bind(this);
+    // this.submitData = this.submitData.bind(this);
     // this.randomPeople = this.randomPeople.bind(this);
   }
 
@@ -66,21 +19,16 @@ class App extends React.Component {
     this.setState({
       peopleNumber: event.target.value
     });
-    // let newNumber;
-    // newNumber = parseInt(Math.random()*this.state.peopleNumber) + 1;
-    // this.setState({ 
-    //   numberGiven: newNumber
-    // });
   }
 
-  submitData(){
-    let newNumber;
-    newNumber = parseInt(Math.random()*this.state.peopleNumber) + 1;
-    this.setState({ 
-      numberGiven: newNumber,
-      text: "submit",
-    });
-  }
+  // submitData(){
+  //   let newNumber;
+  //   newNumber = parseInt(Math.random()*this.state.peopleNumber) + 1;
+  //   this.setState({ 
+  //     numberGiven: newNumber,
+  //     text: "submit",
+  //   });
+  // }
 
   render() {
     return (
@@ -89,13 +37,7 @@ class App extends React.Component {
           {this.state.head_text}
         </header>
         <div class="content">
-          <div class="input">
-            <input value={this.state.peopleNumber} onChange={this.inputPeople} />
-            <button onClick={this.submitData}>{this.state.text}</button>
-          </div>
-          <div class="random">
-            <p>{this.state.numberGiven}</p>
-          </div>
+          <Person change={this.inputPeople} person={this.state.peopleNumber}></Person>
         </div>
       </div>
     );
