091c87894f0864fde1cbe5c0a79aa29580a51ac3
Eve Shi <qianyiwork@gmail.com>
2018-03-02 18:20:10 +0800

    updates

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react';
+import { BrowserRouter } from 'react-router-dom'
 import Layouts from '../../hoc/layouts/layouts';
 import RandomPeople from '../randomPeople/randomPeople';
 import ChooseMovie from '../chooseMovie/chooseMovie';
@@ -8,11 +9,13 @@ class App extends PureComponent {
 
   render() {
     return (
+      <BrowserRouter>
         <Layouts>
           <RandomPeople />
           <ChooseMovie />
         </Layouts>
-    );
+      </BrowserRouter>
+     );
   }
 }
 
